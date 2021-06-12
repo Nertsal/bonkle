9abd8f0eff6e84ae85c8cb18245ce0b67490099d
Alexander <sasha.kudasov04@gmail.com>
2021-06-12 15:01:13 +0500

    Chain

--- src/game/model/update.rs
+++ src/game/model/update.rs
@@ -5,9 +5,17 @@ impl Model {
 
     pub fn fixed_update(&mut self, delta_time: f32) {
         self.move_player(delta_time);
+        self.collide();
     }
 
     fn move_player(&mut self, delta_time: f32) {
         self.player.body.position += self.player.body.velocity * delta_time;
+        self.player.head.position += self.player.head.velocity * delta_time;
+
+        let offset = self.player.head.position - self.player.body.position;
+        let distance = offset.length() - self.player.chain_length;
+        self.player.head.position -= offset.normalize_or_zero() * distance;
     }
+
+    fn collide(&mut self) {}
 }

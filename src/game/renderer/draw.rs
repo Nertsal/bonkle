a485ba3bd4229e4f5271f46c0df787ae1d4083f1
Alexander <sasha.kudasov04@gmail.com>
2021-06-22 16:14:04 +0500

    Smoother circles

--- src/game/renderer/draw.rs
+++ src/game/renderer/draw.rs
@@ -16,8 +16,7 @@ impl Renderer {
                 Effect::Heal { .. } => Color::new(0.0, 1.0, 0.0, 0.5),
             };
             draw_circle(
-                area_effect.position.x,
-                area_effect.position.y,
+                area_effect.position,
                 area_effect.radius,
                 area_color,
             );
@@ -27,8 +26,7 @@ impl Renderer {
         let coefficient = model.player.entity.health.hp_frac();
         let player_life_color = color_alpha(PLAYER_LIFE_COLOR, 0.5);
         draw_circle(
-            model.player.entity.rigidbody.position.x,
-            model.player.entity.rigidbody.position.y,
+            model.player.entity.rigidbody.position,
             model.player.chain_length * coefficient,
             player_life_color,
         );
@@ -36,16 +34,9 @@ impl Renderer {
         // Spawners
         let spawner_color = Color::new(SPAWNER_COLOR.r, SPAWNER_COLOR.g, SPAWNER_COLOR.b, 0.5);
         for spawner in &model.spawners {
-            draw_circle_lines(
-                spawner.position.x,
-                spawner.position.y,
-                spawner.spawn_group.radius,
-                0.2,
-                spawner_color,
-            );
+            draw_circle_outline(spawner.position, spawner.spawn_group.radius, spawner_color);
             draw_circle(
-                spawner.position.x,
-                spawner.position.y,
+                spawner.position,
                 spawner.time_left / spawner.time_left_max * spawner.spawn_group.radius,
                 spawner_color,
             );
@@ -61,19 +52,16 @@ impl Renderer {
             self.draw_rigidbody(&enemy.entity().rigidbody, enemy.entity().color);
             let health_frac = enemy.health_frac();
             draw_circle(
-                enemy.entity().rigidbody.position.x,
-                enemy.entity().rigidbody.position.y,
+                enemy.entity().rigidbody.position,
                 health_frac * enemy.entity().rigidbody.collider.radius,
                 enemy.entity().color,
             );
         }
 
         // Player border
-        draw_circle_lines(
-            model.player.entity.rigidbody.position.x,
-            model.player.entity.rigidbody.position.y,
+        draw_circle_outline(
+            model.player.entity.rigidbody.position,
             model.player.chain_length,
-            0.2,
             PLAYER_BORDER_COLOR,
         );
 
@@ -94,13 +82,7 @@ impl Renderer {
     }
 
     fn draw_rigidbody(&self, rigidbody: &RigidBody, color: Color) {
-        draw_circle_lines(
-            rigidbody.position.x,
-            rigidbody.position.y,
-            rigidbody.collider.radius,
-            0.5,
-            color,
-        );
+        draw_circle_outline(rigidbody.position, rigidbody.collider.radius, color);
     }
 
     fn draw_ui(&self) {
@@ -111,4 +93,12 @@ impl Renderer {
 
 fn color_alpha(color: Color, alpha: f32) -> Color {
     Color::new(color.r, color.g, color.b, alpha)
+}
+
+fn draw_circle(position: Vec2, radius: f32, color: Color) {
+    draw_poly(position.x, position.y, 200, radius, 0.0, color);
+}
+
+fn draw_circle_outline(position: Vec2, radius: f32, color: Color) {
+    draw_poly_lines(position.x, position.y, 200, radius, 0.0, 0.2, color);
 }
